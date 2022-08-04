bfc57d5d788f7a433e7d156c3a442688e341a8f0
Yashraj Mahesh Mohrir <89199535+YashrajM@users.noreply.github.com>
2022-08-04 19:05:21 +0530

    Add files via upload

---
+++ Octoprime.py
@@ -0,0 +1,13 @@
+from abc import ABC
+from car import Car
+import array as arr
+class Carigen(Car,ABC):
+    def __init__(self, last_service_dates):
+        super().__init__(last_service_date)
+         
+        for x in range(i=0):
+          arr[i]=int(input())
+          ele = int(input())
+        if arr[i]>3: 
+              print("Needs To be Serviced")
+          
